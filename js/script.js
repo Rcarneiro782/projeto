2a41da33873bdaade53b513ad34cc55a97261e03
Rafael <rcarneiro782@gmail.com>
2024-05-11 20:48:28 -0300

    js adicionado e criando personangens

--- js/script.js
+++ js/script.js
@@ -1,12 +1,2 @@
-//knight ou sorcerer
-//LittleMonster ou BigMonster
-
-class Character {
-   
-   life = 1;
-   
-   
-    constructor(name){
-        this.name = name;
-    }
-}
+let char =  new Knight('Rafa');
+let monster = new LittleMonster()
