8e3730dfc4413baf9cb22b1afa6e73d43c0b7a10
Rafael <rcarneiro782@gmail.com>
2024-05-11 21:54:41 -0300

    erro na hora da confirmaçao do código

--- js/script.js
+++ js/script.js
@@ -1,2 +1,16 @@
 let char =  new Knight('Rafa');
 let monster = new LittleMonster()
+
+
+
+
+const gameStage = new Stage(
+char,
+monster,
+document.querySelector('#char'),
+document.querySelector('#monster')
+
+
+);
+
+gameStage.start();
